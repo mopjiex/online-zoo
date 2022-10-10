b7a317186803c9f673ddfeab7b2e8c9e4e99a221
mopjiex <artem1bannikov@gmail.com>
2022-10-10 11:33:24 +0300

    Мобильное меню

--- new/script/slider.js
+++
@@ -1,9 +0,0 @@
-const swiper = new Swiper('.pets__inner', {
-    speed: 400,
-    spaceBetween: 100,
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.pets__arrow-next',
-        prevEl: '.pets__arrow-prev',
-      },
-  });
